f405bc007ecf33313f1b027cff5dd926d40b2f89
joao pedro renan <jota.renan@gmail.com>
2021-08-08 19:35:37 -0300

    Adiciona configurações de email

--- superlists/settings.py
+++ superlists/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = '/static/'
 AUTHENTICATION_BACKENDS = [
     'accounts.authentication.PasswordlessAuthenticationBackend',
 ]
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jotarenandcc@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
